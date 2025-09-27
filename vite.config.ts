9287c7cd06568bced0ab4b6d33d894064ddfb18d
Alexander Goodkind <alex@goodkind.io>
2025-09-27 02:22:40 -0700

    Additional init

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@app-types': resolve(__dirname, './src/types'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
